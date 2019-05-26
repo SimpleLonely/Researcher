95f69593db7724411f889076913a9fd3ab5dd380
SimpleLin <171250015@smail.nju.edu.cn>
2019-05-26 10:40:07 +0800

    inital

---
+++ BottomNavigationApplication/app/src/main/java/com/alljedi/bottomnavigationapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.alljedi.bottomnavigationapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+    private ViewPager viewPager;
+
+    private HomeFragment homeFragment = new HomeFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
+    private OtherFragment otherFragment = new OtherFragment();
+
+    BottomNavigationView navView;
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            //点击BottomNavigationView的Item项，切换ViewPager页面
+            //menu/navigation.xml里加的android:orderInCategory属性就是下面item.getOrder()取的值
+            viewPager.setCurrentItem(item.getOrder());
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.addOnPageChangeListener(this);
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return homeFragment;
+                    case 1:
+                        return profileFragment;
+                    case 2:
+                        return otherFragment;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        //页面滑动的时候，改变BottomNavigationView的Item高亮
+        navView.getMenu().getItem(position).setChecked(true);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
